9a29daf7f4137fb32d3fbb5764d414c055394172
ry <ryr4242@gmail.com>
2026-04-04 00:56:51 -0500

    minor sprites update

--- play.pokemonshowdown.com/sprites/find-missing.pkmn.js
+++ play.pokemonshowdown.com/sprites/find-missing.pkmn.js
@@ -11,7 +11,7 @@ const SPECIES = JSON.parse(arr);
 
 const GENS = Object.keys(PokemonSprites.GENS);
 const MISSING = {};
-const LENGTH = 'https://play.pokemonshowdown.com/sprites/'.length;
+const LENGTH = 'https://kaskadeshowdown.dynv6.net/sprites/'.length;
 
 function sprites(gen, namestar) {
   const name = namestar.endsWith('*') ? namestar.slice(0, -1) : namestar;
